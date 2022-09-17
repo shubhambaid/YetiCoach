47fb08cf3ac12ae6219d7e38b330684fe69a4191
shubhambaid <shubhambaid99@gmail.com>
2022-09-17 19:28:25 +0530

    :snowboarder: Add Skii angle calculation and overlays

---
+++ angle_metrics.py
@@ -0,0 +1,62 @@
+import cv2
+import numpy as np
+from math import sqrt,degrees,atan
+
+def get_dist(coords):
+    x1, y1, x2, y2 = (
+        coords[0],
+        coords[1],
+        coords[2],
+        coords[3],
+    )
+    dist = sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    return (dist,[x1,y1,x2,y2])
+
+def calc_angle(coords):
+    x1, y1, x2, y2 = (
+        coords[1][0],
+        coords[1][1],
+        coords[1][2],
+        coords[1][3],
+    )
+    if y1 > y2:
+        slope = (y1-y2)/(x1-x2)
+    else:
+        slope = (y2-y1)/(x2-x1)
+        
+    angle = degrees(atan(slope))
+    return angle
+
+def get_skii_angle(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    kernel_size = 5
+    blur_gray = cv2.GaussianBlur(gray, (kernel_size, kernel_size), 0)
+
+    low_threshold = 50
+    high_threshold = 150
+    edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
+
+    rho = 1
+    theta = np.pi / 180
+    threshold = 15
+    min_line_length = 50
+    max_line_gap = 20
+    line_image = np.copy(img) * 0
+
+    lines = cv2.HoughLinesP(
+        edges, rho, theta, threshold, np.array([]), min_line_length, max_line_gap
+    )
+
+    lengths = []
+    if lines is not None:
+        for line in lines:
+            lengths.append(get_dist(line[0]))
+            for x1, y1, x2, y2 in line:
+                cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 5)
+        return calc_angle(max(lengths))
+    else:
+        return -1
+
+
+
--- position_inference.py
+++ position_inference.py
@@ -1,10 +1,15 @@
+from dis import dis
 import torch
 import cv2
 from math import sqrt
+from angle_metrics import get_skii_angle
+
 
 class Yeti:
     def __init__(self):
-        self.model = torch.hub.load("ultralytics/yolov5", "custom", path="weights/det_2.pt")
+        self.model = torch.hub.load(
+            "ultralytics/yolov5", "custom", path="weights/det_2.pt"
+        )
 
     def plot_boxes(self, det, img, line_thickness=3):
         c1, c2 = (int(det[0]) - 10, int(det[1])), (int(det[2]) + 10, int(det[3]))
@@ -12,53 +17,96 @@ class Yeti:
         cv2.rectangle(
             img, c1, c2, color, thickness=line_thickness, lineType=cv2.LINE_AA
         )
+        if((det[2]-det[0]) > (det[3]-det[1])):
+            self.draw_text(
+                img, "Tilt Detected", (500, 10), (0, 100, 140), (0, 0, 0)
+                )
 
-    def get_centroid(self,coords):
-        return (coords[0]+(coords[2]/2), coords[1]+(coords[3]/2))
-    
-    def get_distance_skii(self,dets):
+
+    def get_centroid(self, coords):
+        return (coords[0] + (coords[2] / 2), coords[1] + (coords[3] / 2))
+
+    def draw_text(
+        self, img, text, pos, text_color=(0, 255, 0), text_color_bg=(0, 0, 0)
+    ):
+        x, y = pos
+        text_size, _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, 2)
+        text_w, text_h = text_size
+        cv2.rectangle(img, pos, (x + text_w, y + text_h + 5), text_color_bg, -1)
+        cv2.putText(
+            img,
+            text,
+            (x, y + text_h + 3 - 1),
+            cv2.FONT_HERSHEY_COMPLEX_SMALL,
+            1,
+            text_color,
+            2,
+        )
+        return text_size
+
+    def get_distance_skii(self, dets, frame):
         centroids = []
         for det in dets:
             centroids.append(self.get_centroid(det))
-        x1,y1,x2,y2 = centroids[0][0],centroids[0][1],centroids[1][0],centroids[1][1]
-        dist = sqrt( (x2 - x1)**2 + (y2 - y1)**2 )
-        return dist
+        x1, y1, x2, y2 = (
+            centroids[0][0],
+            centroids[0][1],
+            centroids[1][0],
+            centroids[1][1],
+        )
+        dist = sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+        if dist < 200:
+            result = "Close"
+        else:
+            result = "Far"
+        self.draw_text(
+            frame, "Separation: {}".format(result), (30, 10), (0, 200, 200), (0, 0, 0)
+        )
+        return frame, dist
 
-    def inference(self,video_path):
-        vid = cv2.VideoCapture(video_path,cv2.CAP_FFMPEG)
-        
+    def inference(self, video_path):
+        vid = cv2.VideoCapture(video_path, cv2.CAP_FFMPEG)
+
         while True:
             ret, frame = vid.read()
             if ret == False:
                 break
-            
+
             detections = []
+            skii_angles = []
             results = self.model(frame)
             if not results.pandas().xyxy[0].empty:
-                        for result in results.pandas().xyxy[0].itertuples():
-                            print(result)
-                            detections.append(
-                                    [
-                                        int(result[1]),
-                                        int(result[2]),
-                                        int(result[3]),
-                                        int(result[4])
-                                    ]
-                                )
-            
-            for detection in detections:
-                self.plot_boxes(detection,frame,3)
-            
-            if len(detections) == 2:
-                distance = self.get_distance_skii(detections)
+                for result in results.pandas().xyxy[0].itertuples():
+                    detections.append(
+                        [int(result[1]), int(result[2]), int(result[3]), int(result[4])]
+                    )
+                    skii_angles.append(get_skii_angle(frame[int(result[2]):int(result[4]),int(result[1]):int(result[3])]))
 
-            cv2.imshow("frame",frame)
+            for detection in detections:
+                self.plot_boxes(detection, frame, 3)
+
+            if len(detections) == 2:
+                frame, distance = self.get_distance_skii(detections, frame)
+            
+            if len(skii_angles)>1:
+                angle = int(sum(skii_angles)/len(skii_angles))
+                if angle > 0:
+                    self.draw_text(
+                        frame, "Angle: {}".format(str(angle)), (30, 50), (0, 200, 200), (0, 0, 0)
+                    )
+                else:
+                    self.draw_text(
+                        frame, "Angle: {}".format("NaN"), (30, 50), (0, 200, 200), (0, 0, 0)
+                    )
+
+            cv2.imshow("frame", frame)
             key = cv2.waitKey(1)
             if key == ord("q"):
                 break
         vid.release()
-        return("[Video Processing Complete]")
-    
+        return "[Video Processing Complete]"
+
+
 if __name__ == "__main__":
     yeti = Yeti()
-    yeti.inference('videos/gp1.mp4')
+    yeti.inference("videos/gp1.mp4")
